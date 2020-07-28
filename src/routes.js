212391361eb9e26d0785d97b3fb9b65de3bfcbec
Wilda Cheng <wildacheng5@gmail.com>
2020-07-28 11:20:32 -0400

    add back blog

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,9 @@ import { connect } from "react-redux";
 import { withRouter, Route, Switch } from "react-router-dom";
 // import {Login, Signup, UserHome} from './components'
 
-import HomePage from "./components";
+// import HomePage from "./components";
 // import ReviewForm from './components'
-// import Blog from './components'
+import { Blog, HomePage } from './components'
 
 class Routes extends React.Component {
   render() {
@@ -13,8 +13,8 @@ class Routes extends React.Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route exact path="/" component={HomePage} />
-        {/* <Route exact path="/reviewform" component={ReviewForm} />
-        <Route exact path="/blog" component={Blog} /> */}
+        {/* <Route exact path="/reviewform" component={ReviewForm} /> */}
+        <Route exact path="/blog" component={Blog} />
       </Switch>
     );
   }
