a6e6745f985e494a55fa5daeaaec3da00bde24eb
Wilda Cheng <wildacheng5@gmail.com>
2020-08-08 23:07:04 -0400

    test

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { Navbar } from './components'
 import Routes from './routes'
 import scriptLoader from 'react-async-script-loader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Routes />
-      </header>
-    </div>
-  );
+function App({ isScriptLoaded, isScriptLoadSucceed}) {
+  if (isScriptLoaded && isScriptLoadSucceed) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Routes />
+        </header>
+      </div>
+    );
+  } else{
+    return (
+      <div className="App">
+      </div>
+    )
+  }
 }
 
-export default scriptLoader([`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_STATIC_MAP}`])(App);
+export default scriptLoader([`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_STATIC_MAP}&libraries=places`])(App);
