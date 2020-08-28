dec81d54f6831cfc15a354ce56bd3dea59b2ebf2
Wilda Cheng <wildacheng5@gmail.com>
2020-08-28 12:20:00 -0400

    Added the travis file and homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Navbar } from './components'
-import Routes from './routes'
-import scriptLoader from 'react-async-script-loader';
+import React from "react";
+import { Navbar } from "./components";
+import Routes from "./routes";
+import scriptLoader from "react-async-script-loader";
 
-function App({ isScriptLoaded, isScriptLoadSucceed}) {
+function App({ isScriptLoaded, isScriptLoadSucceed }) {
   if (isScriptLoaded && isScriptLoadSucceed) {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div>
+        <header>
           <Navbar />
           <Routes />
         </header>
       </div>
     );
-  } else{
-    return (
-      <div className="App">
-      </div>
-    )
+  } else {
+    return <div></div>;
   }
 }
 
-export default scriptLoader([`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_STATIC_MAP}&libraries=places`])(App);
+export default scriptLoader([
+  `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API}&libraries=places`,
+])(App);
